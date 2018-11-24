00bb48c1a1bbafc8b0fc1d2afc70f259e91b7650
Bruno Moraes <bruno.delcio@ulbra.inf.br>
2018-11-24 15:46:21 -0200

    Updates
    Updates

--- src/main/java/br/edu/ulbra/election/voter/exception/GenericOutputException.java
+++ src/main/java/br/edu/ulbra/election/voter/exception/GenericOutputException.java
@@ -4,7 +4,7 @@ public class GenericOutputException extends RuntimeException {
 
 	private static final long serialVersionUID = 1L;
 
-	public GenericOutputException(String message){
-        super(message);
-    }
+	public GenericOutputException(String message) {
+		super(message);
+	}
 }
--- src/main/java/br/edu/ulbra/election/voter/model/Voter.java
+++ src/main/java/br/edu/ulbra/election/voter/model/Voter.java
@@ -1,58 +1,57 @@
 package br.edu.ulbra.election.voter.model;
 
 import javax.persistence.*;
-
 import br.edu.ulbra.election.voter.repository.VoterRepository;
 
 @Entity
 public class Voter {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
 
-    @Column(unique = true, nullable = false)
-    private String email;
+	@Column(unique = true, nullable = false)
+	private String email;
 
-    @Column(nullable = false)
-    private String name;
+	@Column(nullable = false)
+	private String name;
 
-    @Column(nullable = false)
-    private String password;
+	@Column(nullable = false)
+	private String password;
 
-    public Long getId() {
-        return id;
-    }
+	public Long getId() {
+		return id;
+	}
 
-    public void setId(Long id) {
-        this.id = id;
-    }
+	public void setId(Long id) {
+		this.id = id;
+	}
 
-    public String getEmail() {
-        return email;
-    }
+	public String getEmail() {
+		return email;
+	}
 
-    public void setEmail(String email) {
-        this.email = email;
-    }
+	public void setEmail(String email) {
+		this.email = email;
+	}
 
-    public String getName() {
-        return name;
-    }
+	public String getName() {
+		return name;
+	}
 
-    public void setName(String name) {
-        this.name = name;
-    }
+	public void setName(String name) {
+		this.name = name;
+	}
 
-    public String getPassword() {
-        return password;
-    }
+	public String getPassword() {
+		return password;
+	}
 
-    public void setPassword(String password) {
-        this.password = password;
-    }
-    
-    public static boolean verificaEmail(String email, VoterRepository voterRepository) {
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public static boolean verificaEmail(String email, VoterRepository voterRepository) {
 
 		Iterable<Voter> list = voterRepository.findAll();
 
@@ -62,6 +61,5 @@ public class Voter {
 			}
 		}
 		return false;
-
 	}
 }
