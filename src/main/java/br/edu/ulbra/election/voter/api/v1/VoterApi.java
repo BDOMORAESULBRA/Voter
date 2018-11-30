e285eecb810a02b044adffb747df76df5b126474
Bruno Moraes <bruno.delcio@ulbra.inf.br>
2018-11-30 17:12:06 -0200

    Updates and add arqs of login operation.
    Updates and add arqs of login operation.

--- src/main/java/br/edu/ulbra/election/voter/api/v1/VoterApi.java
+++ src/main/java/br/edu/ulbra/election/voter/api/v1/VoterApi.java
@@ -7,7 +7,6 @@ import br.edu.ulbra.election.voter.service.VoterService;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
-
 import java.util.List;
 
 @RestController
